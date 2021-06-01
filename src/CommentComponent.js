07d5f76c3de69001bbdca012b16de32d68c227b7
Ajay <ajay74984@gmail.com>
2021-06-01 08:52:48 +0200

    first commit

---
+++ src/CommentComponent.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const CommentComponent = (props) => {
+    return (
+        <div className="comment">
+            <a href="/" className="avatar">
+                <img alt="avatar" src={props.imageSrc}/>
+            </a>
+            <div className="content">
+                <a href="/" className="author">
+                    {props.author}
+                </a>
+                <div className="metadata">
+                    <span className="date">{props.timeAgo}</span>
+                </div>
+                <div className="text">
+                    {props.commentText}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CommentComponent;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import faker from 'faker'
+import CommentComponent from "./CommentComponent";
+import ApprovalCard from "./ApprovalCard";
+
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard/>
+            <CommentComponent
+                author="Sam"
+                timeAgo="Today at 04:00PM"
+                imageSrc={faker.image.avatar()}
+                commentText="Wow!"
+            />
+            <CommentComponent
+                author="Alex"
+                timeAgo="Today at 10:00PM"
+                imageSrc={faker.image.avatar()}
+                commentText="Amazing!"
+            />
+            <CommentComponent
+                author="Elena"
+                timeAgo="Yesterday at 02:00AM"
+                imageSrc={faker.image.avatar()}
+                commentText="Superb!"
+            />
+        </div>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App/>,
+    document.querySelector('#root'));
