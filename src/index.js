4810bf4a157458f5c868528063248f63c9d62a40
Ajay <ajay74984@gmail.com>
2021-06-03 08:40:30 +0200

    initial

--- src/index.js
+++ src/index.js
@@ -1,35 +1,52 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
 import faker from 'faker'
 import CommentComponent from "./CommentComponent";
 import ApprovalCard from "./ApprovalCard";
+import ReactDOM from "react-dom";
 
-const App = () => {
-    return (
-        <div className="ui container comments">
-            <ApprovalCard/>
-            <CommentComponent
-                author="Sam"
-                timeAgo="Today at 04:00PM"
-                imageSrc={faker.image.avatar()}
-                commentText="Wow!"
-            />
-            <CommentComponent
-                author="Alex"
-                timeAgo="Today at 10:00PM"
-                imageSrc={faker.image.avatar()}
-                commentText="Amazing!"
-            />
-            <CommentComponent
-                author="Elena"
-                timeAgo="Yesterday at 02:00AM"
-                imageSrc={faker.image.avatar()}
-                commentText="Superb!"
-            />
-        </div>
-    );
+class App extends React.Component {
+    state = {lat: null};
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.lat}),
+            err => console.log(err)
+        );
+    }
+
+    render() {
+        return (
+            <div className="ui container comments">
+                <ApprovalCard>
+                    <CommentComponent
+                        author="Sam"
+                        timeAgo="Today at 04:00PM"
+                        imageSrc={faker.image.avatar()}
+                        commentText="Wow!"
+                    />
+                </ApprovalCard>
+                <ApprovalCard>
+                    <CommentComponent
+                        author="Alex"
+                        timeAgo="Today at 10:00PM"
+                        imageSrc={faker.image.avatar()}
+                        commentText="Amazing!"
+                    />
+                </ApprovalCard>
+                <ApprovalCard>
+                    <CommentComponent
+                        author="Elena"
+                        timeAgo="Yesterday at 02:00AM"
+                        imageSrc={faker.image.avatar()}
+                        commentText="Superb!"
+                    />
+                </ApprovalCard>
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(
-    <App/>,
+    <App/>
+    ,
     document.querySelector('#root'));
